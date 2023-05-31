2cd571654ebeab5e7f392ad5f4de93a51d489c7b
Brian Faust <hello@basecode.sh>
2023-05-31 06:52:45 +0300

    ENHANCEMENT: Make `device_id` optional according to specification

--- src/Reference/Player.php
+++ src/Reference/Player.php
@@ -50,59 +50,59 @@ final readonly class Player extends AbstractReference
         return CurrentlyPlaying::from($response);
     }
 
-    public function play(string $deviceId, array $context = []): bool
+    public function play(array $context = []): bool
     {
         return $this
-            ->put('me/player/play', \array_merge(['device_id' => $deviceId], $context))
+            ->put('me/player/play', $context)
             ->status() === 204;
     }
 
-    public function pause(string $deviceId): bool
+    public function pause(array $context = []): bool
     {
         return $this
-            ->put('me/player/pause', ['device_id' => $deviceId])
+            ->put('me/player/pause', $context)
             ->status() === 204;
     }
 
-    public function skipToPrevious(string $deviceId): bool
+    public function skipToPrevious(array $context = []): bool
     {
         return $this
-            ->post('me/player/previous', ['device_id' => $deviceId])
+            ->post('me/player/previous', $context)
             ->status() === 204;
     }
 
-    public function skipToNext(string $deviceId): bool
+    public function skipToNext(array $context = []): bool
     {
         return $this
-            ->post('me/player/next', ['device_id' => $deviceId])
+            ->post('me/player/next', $context)
             ->status() === 204;
     }
 
-    public function seek(string $deviceId, int $positionMs): bool
+    public function seek(int $positionMs, array $context = []): bool
     {
         return $this
-            ->put('me/player/seek', ['device_id' => $deviceId], ['position_ms' => $positionMs])
+            ->put('me/player/seek', $context, ['position_ms' => $positionMs])
             ->status() === 204;
     }
 
-    public function repeatMode(string $deviceId, string $state): bool
+    public function repeatMode(string $state, array $context = []): bool
     {
         return $this
-            ->put('me/player/repeat', ['device_id' => $deviceId], ['state' => $state])
+            ->put('me/player/repeat', $context, ['state' => $state])
             ->status() === 204;
     }
 
-    public function volume(string $deviceId, int $volumePercent): bool
+    public function volume(int $volumePercent, array $context = []): bool
     {
         return $this
-            ->put('me/player/volume', ['device_id' => $deviceId], ['volume_percent' => $volumePercent])
+            ->put('me/player/volume', $context, ['volume_percent' => $volumePercent])
             ->status() === 204;
     }
 
-    public function shuffle(string $deviceId, string $state): bool
+    public function shuffle(string $state, array $context = []): bool
     {
         return $this
-            ->put('me/player/shuffle', ['device_id' => $deviceId], ['state' => $state])
+            ->put('me/player/shuffle', $context, ['state' => $state])
             ->status() === 204;
     }
 
@@ -116,10 +116,10 @@ final readonly class Player extends AbstractReference
         return Queue::from($this->get('me/player/queue')->json());
     }
 
-    public function queueTrack(string $deviceId, string $uri): bool
+    public function queueTrack(string $uri, array $context = []): bool
     {
         return $this->post('me/player/queue', [
-            'device_id' => $deviceId,
+            ...$context,
             'uri' => $uri,
         ])->status() === 204;
     }
